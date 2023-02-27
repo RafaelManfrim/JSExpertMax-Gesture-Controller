a0fc061e490d0d607e026e2eb6b89560eb2e05ac
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2023-02-27 16:19:32 -0300

    corrigindo problema de performace no campo de pesquisa usando workers

---
+++ pages/titles/src/workers/cardListWorker.js
@@ -0,0 +1,16 @@
+onmessage = ({ data }) => {
+  let counter = 0
+  console.log('worker received message', data)
+
+  console.log('activating blocking operation...', data.maxItems)
+  console.time('blocking-op')
+  for (; counter < data.maxItems; counter++) console.log('.')
+  console.timeEnd('blocking-op')
+
+  postMessage({
+    response: "ok",
+    data: {
+      counter
+    }
+  })
+}
